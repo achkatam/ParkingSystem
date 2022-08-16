2cacdb8c85c1cf8cf0565fb79423800a9f323ff9
achkatam <100487082+achkatam@users.noreply.github.com>
2022-08-15 21:53:12 -0400

    Code added

---
+++ ParkingSystem/Controllers/CarController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkingSystem.Controllers
+{
+    public class CarController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            DataAccess.Cars.Add(car);
+
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCar(string plateNumber)
+        {
+            var car = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);
+            //delete the car
+            DataAccess.Cars.Remove(car);
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ ParkingSystem/Data/Models/DataAccess.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ParkingSystem.Data.Models
+{
+    public class DataAccess
+    {
+        public static List<Car> Cars { get; set; } = new List<Car>();
+    }
+}
